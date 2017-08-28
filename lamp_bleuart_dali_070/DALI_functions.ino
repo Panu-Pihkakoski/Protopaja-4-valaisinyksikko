20931af7547b2f3d4ae98c8b165a1072a8f83fd5
khamara1 <klaus.hamara@aalto.fi>
2017-08-28 10:50:17 +0300

    Added current version of the lamp module code

---
+++ lamp_bleuart_dali_070/DALI_functions.ino
@@ -0,0 +1,398 @@
+void sendZero() {
+  digitalWrite(TX_PIN, LOW);
+  delayMicroseconds(delay_1);
+  digitalWrite(TX_PIN, HIGH);
+  delayMicroseconds(delay_2);
+}
+
+void sendOne() {
+  digitalWrite(TX_PIN, HIGH);
+  delayMicroseconds(delay_1);
+  digitalWrite(TX_PIN, LOW);
+  delayMicroseconds(delay_1);
+}
+
+void sendBit(int i) {
+  if(i) {
+    sendOne();
+  } else {
+    sendZero();
+  }
+}
+
+void sendByte(uint8_t cmd) {
+  for(int i = 7; i >= 0; i--) {
+  sendBit( (cmd >> i) & 1);
+  }
+}
+
+void transmit(uint8_t byte1, uint8_t byte2) {
+  sendBit(1);
+  sendByte(byte1);
+  sendByte(byte2);
+  digitalWrite(TX_PIN, LOW);
+}
+
+
+uint8_t receive() {  
+  ret = 0;
+  int startTime = micros();
+  attachInterrupt(digitalPinToInterrupt(RX_PIN), readRX, FALLING);
+  while(micros() - startTime < timeoutMicros) {
+    if(change) {
+      change = false;
+      delayMicroseconds(620);
+      attachInterrupt(digitalPinToInterrupt(RX_PIN), readRX, CHANGE);
+    }
+  }
+  
+  /*
+  Serial.print("HEX: ");
+  Serial.println(ret, HEX);
+  Serial.print("BIN: ");
+  Serial.println(ret, BIN);
+   */
+  detachInterrupt(digitalPinToInterrupt(RX_PIN));
+  return ret;
+}
+
+void readRX() {
+  
+  ret = ret << 1;
+  ret = ret | ((~digitalRead(RX_PIN)) & 1);
+  detachInterrupt(digitalPinToInterrupt(RX_PIN)); 
+  change = true;
+}
+
+void readChange() {
+  change = true;
+  detachInterrupt(digitalPinToInterrupt(RX_PIN));
+}
+
+void initialisation() {
+  delay(500);
+  const int delaytime = 20; //ms
+  long low_longadd = 0x000000;
+  long high_longadd = 0xffffff;
+  long longadd = (low_longadd + high_longadd) / 2;
+  uint8_t rec;
+  
+  transmit(broadcastC, 0b00100000); //reset
+  delay(delaytime);
+  transmit(broadcastC, 0b00100000); //reset
+  delay(delaytime);
+  transmit(broadcastC, 0b00000000); //turn luminaries off
+  delay(delaytime);
+
+  //start initialisaton
+  transmit(0b10100101, 0b00000000); //initialize
+  delay(delaytime);
+  transmit(0b10100101, 0b00000000);
+  delay(delaytime);
+  transmit(0b10100111, 0b00000000); // randomize addresses
+  delay(delaytime);
+  transmit(0b10100111, 0b00000000);
+  delay(delaytime);
+
+
+
+  while(high_longadd - low_longadd > 1) {
+    //long address sent to luminaries
+    Serial.println(high_longadd, HEX);
+    Serial.println(low_longadd, HEX);
+    Serial.println(longadd, HEX);
+    transmitLongAdd(longadd);
+    delay(delaytime);
+    //check if any long address <= the one sent
+    transmit(0b10101001, 0b00000000);//compare
+    delayMicroseconds(1600);
+    attachInterrupt(digitalPinToInterrupt(RX_PIN), readChange, FALLING);
+    int startTime = micros();
+    while(micros() - startTime < timeoutMicros) {
+      if(change) {
+        break; 
+      }
+    }
+    if(change) {
+      change = false;
+      high_longadd = longadd;
+    } else {
+      detachInterrupt(digitalPinToInterrupt(RX_PIN));
+      low_longadd = longadd;
+    }
+    
+    longadd = (low_longadd + high_longadd) / 2;
+    Serial.println("-----------");
+    delay(delaytime);
+  }
+  detachInterrupt(digitalPinToInterrupt(RX_PIN)); //lisatty
+
+  
+  //check if luminary is in high or low longadd
+  transmitLongAdd(low_longadd);
+  delay(delaytime);
+  transmit(0b10101001, 0b00000000); //compare
+  delay(delaytime);
+
+  rec = receive();
+  if(rec != 0) {
+    transmit(0b10110111, 0b00000001); //program short address 000000
+    Serial.println("Low Long Address found. Assigning it short address 000000");
+    
+  } else {
+    transmitLongAdd(high_longadd);
+    delay(delaytime);
+    transmit(0b10110111, 0b00000000); //compare
+    if(receive() != 0){
+      delay(delaytime);
+      transmit(0b10110111, 0b00000001); //program short address 000000
+      Serial.println("High Long Address found. Assigning it short address 000000");
+    } else {
+      Serial.println("Initialisation failed");
+    }
+  }
+  Serial.print("Short address: ");
+  transmit(0b10111011, 0b00000000);
+  Serial.println(receive(), BIN);
+  transmit(0b10101011, 0b00000000); //withdraw the found luminary from initialisation
+  transmit(0b10100001, 0b00000000); //terminate initialisation for all luminaries
+
+  //blink found luminary off and on
+  transmit(0b00000011, 0b00000000); //off
+  delay(500);
+  transmit(0b00000011, 0b00000101); //recall max level
+}
+
+void transmitLongAdd(long longadd) {
+  delay(20);
+  transmit(0b10110001, (longadd >> 16));
+  delay(20);
+  transmit(0b10110011, (longadd >> 8));
+  delay(20);
+  transmit(0b10110101, longadd);
+  delay(20);
+}
+//Goes through the short address space and returns the first found short address
+//returns the valid short address, if no valid short address is found returns -1
+uint8_t scan(){
+  uint8_t add_byte;   //the actual byte sent to DALI
+  uint8_t short_add;  //short address
+
+  //loops through all 64 addresses and sends a query for version number
+  //if a return byte is received, the loop has found a valid short address and the function returns it
+  for(short_add = 0; short_add < 64; short_add++){
+    add_byte = 1 + (short_add << 1); 
+    
+    delay(10);
+    transmit(add_byte, 0x97); //query for version number
+    delayMicroseconds(100);
+    uint8_t response = receive();
+    //Serial.print("Checking short address: " );
+    //Serial.print(add_byte >> 1);
+    //Serial.print(", received: ");
+    //Serial.println(response);
+    
+    if(response != 0){
+      delay(100);
+      transmit(add_byte, 0x05);
+      delay(500);
+      transmit(add_byte, 0x00);
+      //Serial.println("Short address found:");
+      //Serial.println(short_add, BIN);
+      return short_add;
+    }
+  }
+  return -1;
+}
+
+//function to check minimun and maximum levels of the lamp
+void levelCheck(){
+  transmit(0b00000001, 0xA1);
+  //delayMicroseconds(100);
+  maxLevel = receive();
+  delay(100);
+  transmit(0b00000001, 0xA2);
+  //delayMicroseconds(100);
+  minLevel = receive();
+  delay(100);
+  /*Serial.println("min:");
+  Serial.println(minLevel);
+  Serial.println("max:");
+  Serial.println(maxLevel);*/
+}
+
+uint8_t statusCheck(){
+  uint8_t status;
+  transmit(0b00000001, 0x90);
+  delayMicroseconds(100);
+  status = receive();
+  return status;
+}
+
+void fadeToOff(){
+    uint8_t on = 1;
+    while(on != 0){
+      transmit(0x01, 0x93);
+      delayMicroseconds(100);
+      on = receive();
+      delay(1);
+      transmit(0x01, 0x07); // dims one level
+      delay(3);
+       
+  }
+}
+
+//changes colour temperature to value given as kelvins
+void tempChange(int temp){ 
+  uint16_t mirek = 1000000 / temp;
+  uint8_t mirek_upper = mirek >> 8;
+  uint8_t mirek_lower = mirek;
+
+  delay(20);
+  transmit(0xC3, mirek_upper); //data to dtr1
+  delay(20);
+  transmit(0xA3, mirek_lower); //data to dtr0
+  delay(20);  
+  transmit(0b11000001, 0b00001000); // activate application extended commands
+  delay(20);
+  transmit(0x01, 231); //set temporary colour temperature
+  delay(20);
+  transmit(0b11000001, 0b00001000); // activate application extended commands
+  delay(20);  
+  transmit(0x01, 226); //activate
+  delay(20);
+  
+}
+
+uint16_t queryColourTemp(){
+  uint8_t response;
+  delay(10);
+  transmit(0xA3, 2); //data to dtr
+  delay(10);
+  transmit(0b11000001, 0b00001000); // activate application extended commands
+  delay(10);
+  transmit(0x01, 250); //query colour value
+  delayMicroseconds(100);
+  response = receive();
+  
+  delay(10);
+  transmit(0x01, 0x9C); //read dtr1
+  delayMicroseconds(100);
+  response = receive();
+  temp = response << 8;
+  //Serial.print("DTR1: ");
+  //Serial.println(response, BIN);
+  delay(10);
+  transmit(0x01, 152); //read dtr
+  delayMicroseconds(100);
+  response = receive();
+  temp = temp + response;
+  //Serial.print("DTR: ");
+  //Serial.println(response, BIN);
+  
+  //Serial.print("temp: ");
+  //Serial.println(temp);
+  delay(10);
+
+  temp = 1000000 / temp;
+  return temp;
+}
+
+void queryColourTempLimits(){
+  uint8_t response;
+
+  //calucating warmest
+  transmit(0xA3, 130); //data to dtr
+  delay(10);
+  transmit(0b11000001, 0b00001000); // activate application extended commands
+  delay(10);
+  transmit(0x01, 250); //query colour value
+  delayMicroseconds(100);
+  response = receive();
+  
+  delay(10);
+  transmit(0x01, 0x9C); //read dtr1
+  delayMicroseconds(100);
+  response = receive();
+  warmest = response * 256;
+  delay(10);
+  transmit(0x01, 152); //read dtr
+  delayMicroseconds(100);
+  response = receive();
+  warmest = warmest + response;
+  warmest = 1000000 / warmest;
+
+
+  //calculating coolest
+   transmit(0xA3, 128); //data to dtr
+  delay(10);
+  transmit(0b11000001, 0b00001000); // activate application extended commands
+  delay(10);
+  transmit(0x01, 250); //query colour value
+  delayMicroseconds(100);
+  response = receive();
+  
+  delay(10);
+  transmit(0x01, 0x9C); //read dtr1
+  delayMicroseconds(100);
+  response = receive();
+  coolest = response * 256;
+  delay(10);
+  transmit(0x01, 152); //read dtr
+  delayMicroseconds(100);
+  response = receive();
+  coolest = coolest + response;
+  coolest = 1000000 / coolest;
+  coolest += 50;
+}
+
+void queryColourTempCapability() {
+  uint8_t response;
+  
+  transmit(0b11000001, 0b00001000);
+  delay(10);
+  transmit(0x01, 249);
+  delayMicroseconds(100);
+  response = receive();
+  if(bitRead(response, 1)) {
+    colorTempCap = 1;
+  }
+  else {
+    colorTempCap = 0;
+  }
+}
+
+//function that reads temperature from sensors
+float tempRead(int sensor){ 
+  sensors.requestTemperatures();
+  float temp;
+  if(sensor == 0){
+    temp = sensors.getTempCByIndex(0);  
+  }
+  else if(sensor == 1){
+    temp = sensors.getTempCByIndex(1);
+  }
+  else{
+    return -200;
+  }
+  return temp;
+}
+
+void updateSensorTemps() {
+  float sensor0 = tempRead(0);
+  sensor0_int = (uint8_t)sensor0;
+  sensor0_frac = (uint8_t)( (sensor0-sensor0_int)*100 );
+  if (DEBUG) {
+    //Serial.print("Temp 0: ");
+    //Serial.println(sensor0);
+  }
+
+  float sensor1 = tempRead(1);
+  sensor1_int = (uint8_t)sensor1;
+  sensor1_frac = (uint8_t)( (sensor1-sensor1_int)*100 );
+  if (DEBUG) {
+    //Serial.print("Temp 1: ");
+    //Serial.println(sensor1);
+  }
+}
+
---
+++ lamp_bleuart_dali_070/lamp_bleuart_dali_070.ino
@@ -0,0 +1,568 @@
+#include <bluefruit.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+bool DEBUG = true;
+bool sensors_connected = true;
+
+//Bluetooth variables etc.
+int id = -1;
+bool query_sent = false;
+ble_gap_addr_t own_addr;
+unsigned long quer_ref_time;
+unsigned long conn_ref_time;
+const char* name_of_lamp = "BLEL";
+unsigned long update_ref_time;
+bool first_update = false;
+unsigned long scan_ref_time;
+uint16_t c_conn_handle;
+
+//DALI variables etc.
+const int TX_PIN = 15;
+const int RX_PIN = 2;
+const int delay_1 = 412;
+const int delay_2 = 414;
+const int timeoutMicros = 20000;
+volatile uint8_t ret = 0;
+volatile bool change = false;
+const uint8_t broadcastDP = 0b11111110;
+const uint8_t broadcastC = 0b11111111;
+uint8_t maxLevel;
+uint8_t minLevel;
+uint16_t warmest = 0;
+uint16_t coolest = 0;
+uint8_t colorTempCap = 0;
+uint16_t temp = 0;
+bool dali_setup = true;
+
+//Sensor variables
+const uint8_t onewirebus = 11;
+OneWire oneWire(onewirebus);
+DallasTemperature sensors(&oneWire);
+uint8_t sensor0_int = 0;
+uint8_t sensor0_frac = 0;
+uint8_t sensor1_int = 0;
+uint8_t sensor1_frac = 0;
+
+// BLE Client Service (Connection to Peripheral aka next device)
+BLEClientUart clientUart;
+
+// BLE Peripheral Service (Connection to Central aka previous device)
+BLEUart bleUart;
+
+void setup()
+{
+  Serial.begin(115200);
+  
+  //----- DALI SETUP -----
+  pinMode(RX_PIN, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(TX_PIN, OUTPUT);
+  digitalWrite(TX_PIN, HIGH);
+
+  //transmit(0x00, 200);
+  transmit(0b00000000, 0b00000111);
+
+  //----- BLUETOOTH SETUP -----
+  
+  // Enable both peripheral and central
+  Bluefruit.begin(true, true);
+
+  // Set max power. Accepted values are: -40, -30, -20, -16, -12, -8, -4, 0, 4
+  Bluefruit.setTxPower(4);
+  Bluefruit.setName("BLEL");
+  
+  //----- PERIPHERAL CONNECTION SETUP -----
+  
+  // Callbacks for Peripheral
+  Bluefruit.setConnectCallback(connect_callback);
+  Bluefruit.setDisconnectCallback(disconnect_callback);
+
+  // Configure and Start BLE Uart Service
+  bleUart.begin();
+  //bleUart.setRxCallback(handleDataFromCentral); //Didn't work, has to be called separately
+
+  // Set up Advertising Packet
+  setupAdv();
+
+  //----- CENTRAL CONNECTION SETUP -----
+
+  // Callbacks for Central
+  Bluefruit.Central.setConnectCallback(connect_callback_c);
+  Bluefruit.Central.setDisconnectCallback(disconnect_callback_c);
+
+  // Init BLE Central Uart Service
+  clientUart.begin();
+  clientUart.setRxCallback(handleDataFromPeripheral);
+
+  /* Setup Central Scanning
+   * - Enable auto scan if disconnected
+   * - Interval = 100 ms, window = 80 ms
+   * - Filter only accept bleuart service
+   * - Don't use active scan
+   * - Start(timeout) with timeout = 0 will scan forever (until connected)
+   */
+  Bluefruit.Scanner.setRxCallback(scan_callback_c);
+  Bluefruit.Scanner.restartOnDisconnect(false);
+  Bluefruit.Scanner.setInterval(160, 80); // in unit of 0.625 ms
+  Bluefruit.Scanner.filterUuid(bleUart.uuid);
+  Bluefruit.Scanner.useActiveScan(false);
+
+  //Get the bluetooth address of the nRF52 module
+  sd_ble_gap_address_get(&own_addr);
+
+  //----- SENSOR SETUP -----
+  sensors.begin();
+}
+
+void setupAdv(void)
+{
+  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+  Bluefruit.Advertising.addTxPower();
+
+  // Include bleUart 128-bit uuid and name
+  Bluefruit.Advertising.addService(bleUart);
+  
+  //Include name
+  Bluefruit.Advertising.addName();
+
+  Bluefruit.Advertising.restartOnDisconnect(false);
+  Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
+  Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
+}
+
+void setupDALI() {
+  delay(100);
+  //If the address isn't 0000 0000, initialise 
+  if (scan() != B00000000) {
+    delay(10);
+    initialisation();
+  }
+  delay(100);
+  //Memorize the min and max levels of the lamp
+  levelCheck();
+  if (DEBUG) {
+    Serial.print("MIN: ");
+    Serial.println(minLevel);
+    Serial.print("MAX: ");
+    Serial.println(maxLevel);
+  }
+  
+  //Memorize the coolest and warmest light temperatures of the lamp
+  queryColourTempLimits();
+  if (DEBUG) {
+    Serial.print("WARM: ");
+    Serial.println(warmest);
+    Serial.print("COOL: ");
+    Serial.println(coolest);
+  }
+  
+  //Memorize if the color temperature of the lamp can be changed
+  queryColourTempCapability();
+  if (DEBUG) {
+    Serial.print("TC CAP: ");
+    Serial.println(colorTempCap);
+  }
+
+  //Memorize current color temperature
+  queryColourTemp();
+  if (DEBUG) {
+    Serial.print("CURRENT COL TEMP: ");
+    Serial.println(temp);
+  }
+  
+  dali_setup = false;
+}
+
+void loop()
+{
+  if (dali_setup) setupDALI();
+  
+  checkConnectionStatuses();
+  
+  checkUpdateNeed();
+  
+  //handleDataFromSerial();
+  
+  handleDataFromCentral();
+  
+  //handleDataFromPeripheral() works automatically and there is no need to call it separately
+}
+
+void checkConnectionStatuses(void)
+{
+  if (millis()-scan_ref_time > 10000) Bluefruit.Scanner.stop();
+  
+  //Connection to central lost, disconnect from peripheral
+  if ( !Bluefruit.connected()&& Bluefruit.Central.connected() ) {
+    Bluefruit.Central.disconnect(c_conn_handle);
+  }
+  //Connection to central established, start scanning for peripherals & connect
+  else if ( !Bluefruit.Scanner.isRunning() && millis()-scan_ref_time > 20000 && Bluefruit.connected() && !Bluefruit.Central.connected() ) {
+    Bluefruit.Advertising.stop();
+    Bluefruit.Scanner.start(0); //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    Serial.println("Scanning");
+    scan_ref_time = millis();
+  }
+  //No connections, start advertising
+  else if ( !Bluefruit.Advertising.isRunning() && !Bluefruit.connected() && !Bluefruit.Central.connected() ) {
+    Bluefruit.Scanner.stop();
+    Bluefruit.Advertising.start(0);
+    Serial.println("Advertising");
+  }
+
+  //Query for id if connected to central and no id and query hasn't been sent
+  if (Bluefruit.connected() && id == -1 && !query_sent && millis()-conn_ref_time > 5000) {
+    uint8_t query[64];
+    memset(query, 0, 63);
+
+    query[0] = 'Q';                 //Query type
+    query[1] = own_addr.addr[3];    //Address byte 1
+    query[2] = own_addr.addr[4];    //Address byte 2
+    query[3] = own_addr.addr[5];    //Address byte 3
+    query[4] = '!';                 //End message
+    bleUart.write( query, 5 );
+    query_sent = true;
+    quer_ref_time = millis();
+    if (DEBUG) {Serial.println("Query for id sent");}
+  }
+  //If no answer to query in 5 seconds, send a new one
+  if (query_sent && millis()-quer_ref_time > 5000) {
+    query_sent = false;
+  }
+  
+  if ( Bluefruit.connected() ) {
+    digitalWrite(LED_RED, HIGH);
+  } else {
+    digitalWrite(LED_RED, LOW);
+  }
+}
+
+void checkUpdateNeed() {
+  if ( first_update || (id > -1 && Bluefruit.connected() && millis()-update_ref_time > 10000) ) {
+    uint8_t update[64];
+    memset(update, 0, 63);
+
+    //If updating for the first time, send constants first
+    if (first_update) {
+      update[0] = 'C';                        //Constant type
+      update[1] = id;                         //id of the lamp
+      update[2] = minLevel;                   //min light level of the lamp
+      update[3] = maxLevel;                   //max light level of the lamp
+      update[4] = colorTempCap;               //can the color temp of the lamp be changed (1 or 0)
+      update[5] = (uint8_t)(coolest/100);     //coolest light temp of the lamp divided by 100
+      update[6] = (uint8_t)(warmest/100);     //warmest light temp of the lamp divided by 100
+      update[7] = '!';                        //End message
+
+      bleUart.write( update, 8 );
+
+      if (DEBUG) Serial.println("Sent constants");
+    }
+    memset(update, 0, 63);
+
+    if (sensors_connected) updateSensorTemps();
+    
+    update[0] = 'U';                  //Update type
+    update[1] = id;                   //id of the lamp
+    transmit(B00000001, B10010000);
+    delayMicroseconds(100);
+    update[2] = receive();            //Status byte
+    transmit(B00000001, B10100000);
+    delayMicroseconds(100);
+    update[3] = receive();            //Current level byte
+    update[4] = (uint8_t)(temp/100);  //Current color temp byte
+    update[5] = sensor0_int;          //Integer part of sensor 0 reading
+    update[6] = sensor0_frac;         //Fraction part of sensor 0 reading
+    update[7] = sensor1_int;          //Integer part of sensor 1 reading
+    update[8] = sensor1_frac;         //Fraction part of sensor 1 reading
+    update[9] = '!';                  //End message
+    bleUart.write( update, 10 );
+
+    if (DEBUG) Serial.println("Sent update");
+    
+    first_update = false;
+    update_ref_time = millis();
+  }
+}
+
+void handleDataFromSerial(void)
+{
+  // Forward data from HW Serial to Central (BLEUART) and Peripheral (CLIENTUART)
+  while (Serial.available())
+  {
+    // Delay to wait for enough input, since we have a limited transmission buffer
+    delay(2);
+
+    uint8_t buf[64];
+    int count = Serial.readBytes(buf, sizeof(buf));
+    
+    //Send data to central
+    bleUart.write( buf, count );
+
+    if ( Bluefruit.Central.connected() && clientUart.discovered() )
+    {
+      // Discovered means peripheral connection is in working state
+      // Send data to peripheral
+      clientUart.write(buf, count);
+    }
+  }
+}
+
+void handleDataFromCentral(void)
+{
+  // Forward from Central (BLEUART) to HW Serial and possible Peripheral (CLIENTUART)
+
+  uint8_t buf2[64];
+  uint16_t idx = 0;
+  uint8_t ch;
+  memset(buf2, 0, 63);
+  
+  while ( bleUart.available() )
+  {
+    ch = (uint8_t) bleUart.read();
+    if (ch == '!') {
+      buf2[idx] = ch;
+      idx++;
+      break;
+    }
+    else {
+      buf2[idx] = ch;
+      idx++;
+    }
+  }
+  //RESPONSE: Message is a response to a query from a lamp in the chain
+  if (buf2[0] == 'R') {
+    
+    //If receiver adress bytes match, process the message
+    if (id == -1 &&
+        buf2[1] == own_addr.addr[3] && 
+        buf2[2] == own_addr.addr[4] && 
+        buf2[3] == own_addr.addr[5]) {
+      
+      id = buf2[4]; //Memorize the assigned id
+      update_ref_time = millis();
+      first_update = true;
+      
+      if (DEBUG) {
+        Serial.print("Received ID: ");
+        Serial.println(id);
+      }
+    }
+    //Otherwise this is for someone else, forward the message
+    else {
+      if ( Bluefruit.Central.connected() && clientUart.discovered() )
+      {
+      // Peripheral connection is in working state
+      // Forward data to Peripheral
+      clientUart.write(buf2, idx);
+      }
+    }
+  }
+  //DALI COMMAND: Transmit the DALI command to lamp if the id matches, otherwise forward it
+  else if (buf2[0] == 'D') {
+    if (buf2[1] == id) {
+      if (DEBUG) {Serial.println("Received DALI Command");}
+      transmit(buf2[2], buf2[3]);
+      //Check if we should expect a reply from the lamp
+      if (bitRead(buf2[2],0) == 1) {
+        delayMicroseconds(100);
+        receive();
+        
+        buf2[0] = 'A';
+        buf2[1] = ret;
+        buf2[2] = '!';
+        if ( Bluefruit.connected() ) bleUart.write( buf2, 3 );
+      }
+    }
+    else {
+      if ( Bluefruit.Central.connected() && clientUart.discovered() )
+      {
+      // Peripheral connection is in working state
+      // Forward data to Peripheral
+      clientUart.write(buf2, idx);
+      }
+    }
+  }
+  //BROADCAST: Transmit the DALI command to lamp and forward it to the peripheral
+  else if (buf2[0] == 'B') {
+
+    if ( Bluefruit.Central.connected() && clientUart.discovered() )
+    {
+    // Peripheral connection is in working state
+    // Forward data to Peripheral
+    clientUart.write(buf2, idx);
+    }
+    transmit(B10000000, buf2[1]);
+    
+    if ( Bluefruit.connected() ) bleUart.write( buf2, 3 );
+  }
+  //TEMPERATURE: Transmit the temperature to lamp if it can change color temp and
+  //             the id matches or it's 255, otherwise forward it
+  else if (buf2[0] == 'T') {
+    if ( colorTempCap == 1 && (buf2[1] == id || buf2[1] == 255) ) {
+      temp = buf2[2]*100;
+      tempChange( temp );
+      if (DEBUG) {
+        Serial.print("Changed temp to ");
+        Serial.println(temp);
+        Serial.print("Actual temp is (roughly) ");
+        delay(100);
+        Serial.println(queryColourTemp());
+        update_ref_time = 0;
+      }
+    }
+    
+    if (buf2[1] != id) {
+      if ( Bluefruit.Central.connected() && clientUart.discovered() ) {
+        // Peripheral connection is in working state
+        // Forward data to Peripheral
+        clientUart.write(buf2, 4);
+      }
+    }
+  }
+
+  //Forward data to HW Serial
+  /*if (DEBUG) {
+    Serial.write(buf2, idx);
+  }*/
+}
+
+void handleDataFromPeripheral(BLEClientUart& cent_uart)
+{
+  uint8_t buf3[64];
+  uint16_t idx = 0;
+  uint8_t ch;
+  memset(buf3, 0, 63);
+
+  //Read data from Peripheral
+  while ( clientUart.available() )
+  {
+    ch = (uint8_t) clientUart.read();
+    if (ch == '!') {
+      buf3[idx] = ch;
+      idx++;
+      break;
+    }
+    else {
+      buf3[idx] = ch;
+      idx++;
+    }
+  }
+
+  //Forward data to Central
+  bleUart.write( buf3, idx );
+}
+
+void scan_callback_c(ble_gap_evt_adv_report_t* report)
+{
+  uint8_t len = 0;
+  uint8_t const* data = extractScanData(report->data, report->dlen, BLE_GAP_AD_TYPE_COMPLETE_LOCAL_NAME, &len);
+  bool is_lamp = false;
+
+  //Check if the scan report is from a BLE lamp
+  while( len ) {
+    // found matched
+    if ( !memcmp(data, (const uint8_t*)name_of_lamp, sizeof(name_of_lamp)) ) {
+      is_lamp = true;
+      break;
+    }
+    else {
+      data += sizeof(name_of_lamp);
+      len  -= sizeof(name_of_lamp);
+    }
+  }
+  // Check if the lamp advertising contains BleUart service
+  if ( is_lamp )
+  {
+    if (DEBUG) {
+      Serial.println("BLE Lamp detected");
+      Serial.println("Attempt to connect to peripheral ... ");
+    }
+
+    // Connect to device with bleUart service in advertising
+    // Use Min & Max Connection Interval default value
+    Bluefruit.Central.connect(report);
+  }
+}
+
+void connect_callback_c(uint16_t conn_handle)
+{
+  if (DEBUG) {Serial.println("Connected to peripheral");}
+
+  if (DEBUG) {Serial.print("Discovering BLE Uart Service ... ");}
+
+  if ( clientUart.discover(conn_handle) )
+  {
+    if (DEBUG) {
+      Serial.println("Found it");
+      Serial.println("Enable TXD's notify");
+    }
+    clientUart.enableTXD();
+    c_conn_handle = conn_handle;
+    if (DEBUG) Serial.println("Ready to receive from peripheral");
+  }
+  else
+  {
+    if (DEBUG) {Serial.println("Found NONE, disconnecting");}
+    Bluefruit.Central.disconnect(conn_handle);
+  }
+}
+
+void disconnect_callback_c(uint16_t conn_handle, uint8_t reason)
+{
+  (void) conn_handle;
+  (void) reason;
+  if (DEBUG) {
+  Serial.println("Disconnected from peripheral");
+  }
+}
+
+void connect_callback(uint16_t conn_handle)
+{
+  if (DEBUG) {Serial.println("Connected to central");}
+  conn_ref_time = millis();
+}
+
+void disconnect_callback(uint16_t conn_handle, uint8_t reason)
+{
+  (void) reason;
+  id = -1;
+  quer_ref_time = millis();
+  query_sent = false;
+  if (DEBUG) {
+  Serial.println();
+  Serial.println("Disconnected from central");
+  Serial.println("Bluefruit will auto start advertising (default)");
+  }
+}
+
+uint8_t* extractScanData(uint8_t const* scandata, uint8_t scanlen, uint8_t type, uint8_t* result_len)
+{
+  *result_len = 0;
+
+  // len (1+data), type, data
+  while ( scanlen )
+  {
+    if ( scandata[1] == type )
+    {
+      *result_len = scandata[0]-1;
+      return (uint8_t*) (scandata + 2);
+    }
+    else
+    {
+      scanlen  -= (scandata[0] + 1);
+      scandata += (scandata[0] + 1);
+    }
+  }
+
+  return NULL;
+}
+
+void rtos_idle_callback(void)
+{
+  // Don't call any other FreeRTOS blocking API()
+  // Perform background task(s) here
+
+  // Request CPU to enter low-power mode until an event/interrupt occurs
+  //waitForEvent();
+}
